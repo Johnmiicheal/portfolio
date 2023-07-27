51508e37d0bc91d4e078cb3a54e880b942f20bbf
johnmiicheal <michealelijah301@gmail.com>
2023-07-27 16:26:22 +0100

    stack?

--- pages/index.tsx
+++ pages/index.tsx
@@ -196,7 +196,7 @@ const Home = () => {
 
                 <Box px={5} pt={2} pb={5} bg="white" mt={-20} zIndex="2">
                   I am a hobbyist photographer 📷 and an experimental interface
-                  designer✨, with a touch of graphic and poster designs
+                  designer✨.<br/> Tech Stack: Next.js | Typescript | Node.js | GraphQL | Figma 
                 </Box>
               </Flex>
             </Flex>
