ad2959b2906a44973b8c331add5b3f245eb763fd
Elijah Johnmicheal <michealelijah301@gmail.com>
2023-09-19 14:20:26 +0100

    Update PDFViewer.tsx: updated Resume

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -35,7 +35,7 @@ export const PDFViewer = ({ isOpen, onClose, path }: any) => {
                 </Flex>
                 <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.8.162/build/pdf.worker.min.js">
                     <Viewer
-                    fileUrl="/01_Johnmicheal_Elijah_2023[Main].pdf"
+                    fileUrl="/01_Johnmicheal_Elijah_Resume[Main].pdf"
                     onZoom={handleZoom}
                     plugins={[zoomPluginInstance]}
                     />
